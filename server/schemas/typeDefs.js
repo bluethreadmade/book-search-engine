24771308cd696eac8fd192dfe33ae07cf8a59042
Megan McNeill <mcneim2@gmail.com>
2024-08-13 22:29:59 -0400

    added types, queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,59 @@
 // typedefs go here
+const typeDefs = `
+type: User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Number
+    savedBooks: [Book]
+}
+
+type: Book{
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type: Auth{ 
+  token: ID
+  user: User
+}
+
+type Query {
+  me: User
+}
+
+type SaveBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+}
+
+type Mutation {
+  addUser(username: String!, email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+  saveBook(input: SaveBookInput): User
+  removeBook(bookId: String): User
+
+  createProject(
+    name: String!,
+    materials: [String]!,
+    instructions: [String]!,
+    pricePoint: String!,
+    difficulty: String!,
+    craft: String!
+    authorId: ID!
+  ): Project
+  deleteProject(id: ID!): Project
+
+}
+`;
+
+/* EXPORT */
+module.exports = typeDefs;
