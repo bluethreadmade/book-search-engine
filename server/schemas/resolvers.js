0e8c031cc0e2d49e9e8294f2f619aedfd3a5eb69
Megan McNeill <mcneim2@gmail.com>
2024-08-13 22:53:53 -0400

    added createUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 // resolvers go here
-const { User, Book } = require("../models");
+const { User } = require("../models");
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -18,7 +18,65 @@ const resolvers = {
     },
 
     Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
 
+            if (!user) {
+                throw new Error('Something is wrong!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { username, email, password }) => {
+            const user = await User.findOne({ $or: [{ username }, { email }] });
+
+            if (!user) {
+                throw new Error("Can't find this user");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new Error('Wrong password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true, runValidators: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new Error('You need to be logged in!');
+        },
+
+        deleteBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+
+                if (!updatedUser) {
+                    throw new Error("Couldn't find user with this id!");
+                }
+
+                return updatedUser;
+            }
+
+            throw new Error('You need to be logged in!');
+        },
     }
 }
 
