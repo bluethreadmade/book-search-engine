3608ace26776240db04e14cafd227be213328e0b
Megan McNeill <mcneim2@gmail.com>
2024-08-13 22:46:52 -0400

    adding queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,25 @@
-// resolver go here
+// resolvers go here
+const { User, Book } = require("../models");
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        user: async (parent, { id, username }) => {
+            const query = id ? { _id: id } : { username };
+
+            const foundUser = await User.findOne(query);
+
+            if (!foundUser) {
+                throw new Error('Cannot find a user with this id or username!');
+            }
+
+            return foundUser;
+        },
+    },
+
+    Mutation: {
+
+    }
+}
+
+module.exports = resolvers;
